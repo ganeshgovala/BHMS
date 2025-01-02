f701f9a42620c5c7ae5ddab8fe6dd9036ec05307
ganeshgovala <sriramganesh28@gmail.com>
2025-01-02 15:25:00 +0530

    My First Commit

---
+++ android/app/src/main/kotlin/com/example/bhms/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bhms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello", style: TextStyle(
+            fontSize: 50,
+          ),),
+        )
+      ),
+    );
+  }
+}

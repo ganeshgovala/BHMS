f7c00c30481a1b67f220e3309539c2f4023981cd
ganeshgovala <sriramganesh28@gmail.com>
2025-01-03 16:58:55 +0530

    Modified Text Styles in OpeningPage

--- lib/pages/OpeningPage.dart
+++ lib/pages/OpeningPage.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bhms/components/CustomTextStyles.dart';
+import 'package:bhms/pages/LoginPage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -24,28 +26,26 @@ class OpeningPage extends StatelessWidget {
               padding: EdgeInsets.only(left: 20, right: 20),
               child: Column(
                 children: [
-                  Text("Welcome to the App", style: GoogleFonts.poppins(
-                    fontSize: 28,
-                    color: const Color(0xFF2F2E41),
-                    fontWeight: FontWeight.w700,
-                  )),
+                  Text("Welcome to the App", style: CustomTextStyles().heading(),),
                   SizedBox(height: 15),
-                  Text("Your one stop solution for monitoring battery health and water levels with ease", style: GoogleFonts.poppins(
-                    fontSize: 13,
-                    color: Colors.grey[700],
-                  ), textAlign: TextAlign.center,),
+                  Text("Your one stop solution for monitoring battery health and water levels with ease", style: CustomTextStyles().subHeading(), textAlign: TextAlign.center,),
                   SizedBox(height: 30,),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 36, vertical: 16),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF4B4B87),
-                      borderRadius: BorderRadius.circular(14)
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 36, vertical: 16),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF4B4B87),
+                        borderRadius: BorderRadius.circular(14)
+                      ),
+                      child: Text("Get Started", style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      )),
                     ),
-                    child: Text("Get Started", style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    )),
                   )
                 ],
               ),
